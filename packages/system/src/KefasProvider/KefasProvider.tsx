6b7b57404e4a31dd603e707c0156fd8b071d8772
Mieszko Sabo <mieszko.mac@gmail.com>
2021-10-28 16:58:28 +0200

    kefas provider update

--- packages/system/src/KefasProvider/KefasProvider.tsx
+++ packages/system/src/KefasProvider/KefasProvider.tsx
@@ -1,12 +1,33 @@
 import { SanitizeCSS } from "@kefas-ui/sanitize-css";
 import * as React from "react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 
 export type KefasProviderProps = {
   sanitize?: boolean;
+  children?: React.ReactNode;
+  theme?: any; // TODO: create theme object type
 };
 
-export const KefasProvider = ({ sanitize }: KefasProviderProps) => (
-  <ThemeProvider theme={{}}>{sanitize && <SanitizeCSS />}</ThemeProvider>
-);
+export const KefasProvider = ({
+  sanitize,
+  theme,
+  children,
+}: KefasProviderProps) => {
+  const globalStyles = theme?.global;
+  const GlobalStyles =
+    globalStyles != null ? createGlobalStyle(globalStyles) : null;
+  console.log(globalStyles);
+  return (
+    <ThemeProvider theme={theme}>
+      {sanitize && <SanitizeCSS />}
+      {GlobalStyles != null && <GlobalStyles />}
+      {children}
+    </ThemeProvider>
+  );
+};
+
+KefasProvider.defaultProps = {
+  sanitize: true,
+  theme: {},
+};
 // TODO: add default theme
